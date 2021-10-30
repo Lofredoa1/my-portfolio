de6de74b1213a9dcb53348657a944efbfae9763e
Anthony Lofredo <anthony.lofredo@gmail.com>
2021-10-30 14:33:01 -0400

    sticky nav

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Particles from 'react-particles-js';
 import "./home.css";
 import particlesConfig from '../../config/particles-config';
+import Navbar from '../navbar/Navbar';
 
 const Home = (props) => {
-    return (
+    return (<div>
+
         <div class="home-container">
         <Particles params={particlesConfig}/>
                 <h1>Hello, I'm Anthony</h1>
                 <h2>I am a full-stack web developer.</h2>
                 <a href="#about">Check out my work</a>
             </div>
+            <Navbar/>
+    </div>
 
     )
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './navbar.css'
 
+
 const Navbar = (props) => {
-    return (
-        <div>
-            <h1>Navbar Component</h1>
-        </div>
-    )
+    
+    const [isSticky, setSticky] = useState(false);
+    let h = window.innerHeight;
+    
+    const handleScroll = () => {
+        //console.log("top:", ref.current.getBoundingClientRect().top)
+        let scrollLength = window.scrollY
+        console.log("new length:", scrollLength)
+        
+      if (scrollLength >= h) {
+        setSticky(true);
+      } else {
+          setSticky(false)
+    
+      }
+    };
+
+
+  
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+       
+
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll);
+    };
+    }, [])
+
+return (
+    <div class={`sticky-wrapper${isSticky ? ' sticky fixed' : ''}`} >
+        <h1 class="sticky-inner">Navbar Component</h1>
+    </div>
+
+)
 }
 
 export default Navbar
