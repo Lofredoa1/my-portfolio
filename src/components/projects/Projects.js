e4f6a30ff0989332d544870ebe29921481e2dd11
Anthony Lofredo <anthony.lofredo@gmail.com>
2021-11-03 21:19:45 -0400

    contact complete

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import "./projects.css"
+import React from "react";
+import "./projects.css";
+import Slide from "react-reveal/Slide";
 
 const Projects = () => {
-    return (
-        <section id="projects">
-            <div>
-                <h1>Projects Component</h1>
-            </div>
+  return (
+    <section id="projects">
+      <div class="projects-container">
+        <Slide left>
+          <h1>PROJECTS</h1>
+        </Slide>
+        <div class="project">
+          <img src="/plantie.png" alt="" />
+          <h4>Plantie</h4><br/>
+          <p>A full-stack MERN app built with one teammate featuring user auth, API calls, task feed based on user inputs. I was responsible for designing and creating the full React architecture as well as helping out with data logic on the backend. Try it out, testuser and testpassword are the credentials.</p>
+          <div>
+              <a href="">Code</a>
+              <a href="">Live</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-        </section>
-    )
-}
-
-export default Projects
+export default Projects;
